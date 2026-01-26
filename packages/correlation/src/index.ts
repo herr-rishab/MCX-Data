9229a6d0effb76ccded34904779eaaba99162b21
ayandafadar <technoayan55@gmail.com>
2026-01-26 23:11:12 +0530

    Added API service for alert ingestion

---
+++ packages/correlation/src/index.ts
@@ -0,0 +1,8 @@
+export function correlate() {
+  console.log("Correlating alerts with accessibility results...");
+
+  // Example rule
+  console.log(
+    "Login service issue detected with accessibility impact – High Priority"
+  );
+}
